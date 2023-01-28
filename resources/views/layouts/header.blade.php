44822e3ddff035defaf7d2f1683af1027aa4a7a9
Scalavia <ihzamahendra4814@gmail.com>
2023-01-28 14:23:42 +0700

    update layouts

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,48 +5,36 @@
             <svg class="icon icon-lg">
                 <use xlink:href="{{ asset('core-ui') }}/vendors/@coreui/icons/svg/free.svg#cil-menu"></use>
             </svg>
-        </button><a class="header-brand d-md-none" href="#">
+        </button>
+        <a class="header-brand d-md-none" href="#">
             <svg width="118" height="46" alt="CoreUI Logo">
                 <use xlink:href="{{ asset('core-ui') }}/assets/brand/coreui.svg#full"></use>
-            </svg></a>
-        <!-- <ul class="header-nav d-none d-md-flex">
-            <li class="nav-item"><a class="nav-link" href="#">Dashboard</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">Users</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">Settings</a></li>
-        </ul> -->
-        <!-- <ul class="header-nav ms-auto">
-            <li class="nav-item"><a class="nav-link" href="#">
-                    <svg class="icon icon-lg">
-                        <use xlink:href="{{ asset('core-ui') }}/vendors/@coreui/icons/svg/free.svg#cil-bell"></use>
-                    </svg></a></li>
-            <li class="nav-item"><a class="nav-link" href="#">
-                    <svg class="icon icon-lg">
-                        <use xlink:href="{{ asset('core-ui') }}/vendors/@coreui/icons/svg/free.svg#cil-list-rich"></use>
-                    </svg></a></li>
-            <li class="nav-item"><a class="nav-link" href="#">
-                    <svg class="icon icon-lg">
-                        <use xlink:href="{{ asset('core-ui') }}/vendors/@coreui/icons/svg/free.svg#cil-envelope-open">
-                        </use>
-                    </svg></a></li>
-        </ul> -->
+            </svg>
+        </a>
         <ul class="header-nav ms-3">
-            <li class="nav-item dropdown"><a class="nav-link py-0" data-coreui-toggle="dropdown" href="#"
-                    role="button" aria-haspopup="true" aria-expanded="false">
+            <p class="mt-2">Selamat Datang, <b>{{ auth()->user()->name }}</b></p>
+            <li class="nav-item dropdown">
+                <a class="nav-link py-0" data-coreui-toggle="dropdown" href="#" role="button"
+                    aria-haspopup="true" aria-expanded="false">
                     <div class="avatar avatar-md"><img class="avatar-img"
                             src="{{ asset('core-ui') }}/assets/img/avatars/8.jpg" alt="user@email.com"></div>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end pt-0">
                     <div class="dropdown-header bg-light py-2">
                         <div class="fw-semibold">Setting</div>
-                    </div><a class="dropdown-item" href="#">
+                    </div>
+                    <a class="dropdown-item" href="#">
                         <svg class="icon me-2">
                             <use xlink:href="{{ asset('core-ui') }}/vendors/@coreui/icons/svg/free.svg#cil-user"></use>
-                        </svg> Profile</a><a class="dropdown-item" href="#">
+                        </svg> Profile
+                    </a>
+                    <a class="dropdown-item" href="{{ route('logout') }}">
                         <svg class="icon me-2">
                             <use
                                 xlink:href="{{ asset('core-ui') }}/vendors/@coreui/icons/svg/free.svg#cil-account-logout">
                             </use>
-                        </svg> Logout</a>
+                        </svg> Logout
+                    </a>
                 </div>
             </li>
         </ul>
