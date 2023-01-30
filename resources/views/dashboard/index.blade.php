fea19fd37964bfc8cd6d0c2790b305cf7a8f0d3e
Scalavia <ihzamahendra4814@gmail.com>
2023-01-30 07:53:09 +0700

    remove sifat surat in dahsboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -79,31 +79,6 @@
                 </div>
             </div>
             <!-- /.col-->
-            <div class="col-sm-6 col-lg-3">
-                <div class="card mb-4 text-white bg-danger">
-                    <div class="card-body pb-0 d-flex justify-content-between align-items-start">
-                        <div>
-                            <div class="fs-1 fw-semibold">{{ $jmlsftsrt }} <span class="fs-6 fw-normal"></span></div>
-                            <div class="h4">Sifat Surat</div>
-                        </div>
-                        <div class="dropdown">
-                            <button class="btn btn-transparent text-white p-0" type="button"
-                                data-coreui-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <svg class="icon">
-                                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-options"></use>
-                                </svg>
-                            </button>
-                            <div class="dropdown-menu dropdown-menu-end"><a class="dropdown-item" href="#">Action</a><a
-                                    class="dropdown-item" href="#">Another action</a><a class="dropdown-item"
-                                    href="#">Something else here</a></div>
-                        </div>
-                    </div>
-                    <div class="c-chart-wrapper mt-3 mx-3" style="height:70px;">
-                        <canvas class="chart" id="card-chart4" height="70"></canvas>
-                    </div>
-                </div>
-            </div>
-            <!-- /.col-->
         </div>
 
         <div class="alert alert-success" role="alert">
@@ -199,31 +174,6 @@
                         </div>
                     </div>
                     <div class="c-chart-wrapper mt-3" style="height:70px;">
-                    </div>
-                </div>
-            </div>
-            <!-- /.col-->
-            <div class="col-sm-6 col-lg-3">
-                <div class="card mb-4 text-white bg-danger">
-                    <div class="card-body pb-0 d-flex justify-content-between align-items-start">
-                        <div>
-                            <div class="fs-1 fw-semibold">{{ $jmlsftsrt }} <span class="fs-6 fw-normal"></span></div>
-                            <div class="h4">Sifat Surat</div>
-                        </div>
-                        <div class="dropdown">
-                            <button class="btn btn-transparent text-white p-0" type="button"
-                                data-coreui-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <svg class="icon">
-                                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-options"></use>
-                                </svg>
-                            </button>
-                            <div class="dropdown-menu dropdown-menu-end"><a class="dropdown-item" href="#">Action</a><a
-                                    class="dropdown-item" href="#">Another action</a><a class="dropdown-item"
-                                    href="#">Something else here</a></div>
-                        </div>
-                    </div>
-                    <div class="c-chart-wrapper mt-3 mx-3" style="height:70px;">
-                        <canvas class="chart" id="card-chart4" height="70"></canvas>
                     </div>
                 </div>
             </div>
